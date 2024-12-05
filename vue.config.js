0d0bda3a74014b1e2c6555f957e022726bffa1f8
jfscha <jfschallibaum@gmail.com>
2024-12-05 12:08:56 -0300

    add: pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     name: "Poke search",
     themeColor: "#FF0000",
     tileColor: "#FFF",
+    manifestPath: "site.webmanifest",
+    manifestOptions: {
+      name: "Poke search",
+      short_name: "Poke",
+      start_url: ".",
+      display: "standalone",
+      orientation: "portrait",
+      theme_color: "#FF0000",
+    },
+    iconPaths: {
+      faviconSVG: "img/icons/favicon.svg",
+      favicon96: "img/icons/favicon-96x96.png",
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/msapplication-icon-144x144.png",
+    }
   }
-})
+});
